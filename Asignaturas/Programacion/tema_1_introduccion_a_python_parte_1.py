11a3edc068a2fc4c981dbd54b238466789cfd71c
Javier López Arce <javier.lopez@solvam.es>
2026-09-22 21:03:51 +0200

    Se añaden enunciados de ejercicios para resolver en python

---
+++ Asignaturas/Programacion/tema_1_introduccion_a_python_parte_1.py
@@ -0,0 +1,51 @@
+# ------------------------------------------------------------------------------
+# EJERCICIO 5
+# Dadas dos variables numéricas A y B, que el usuario debe teclear, se pide
+# realizar un algoritmo que intercambie los valores de ambas variables y muestre
+# cuánto valen al final las dos variables.
+# ------------------------------------------------------------------------------
+
+
+
+
+# ------------------------------------------------------------------------------
+# EJERCICIO 6
+# Algoritmo que lea dos números, calculando y escribiendo el valor de su suma,
+# resta, producto y división.
+# ------------------------------------------------------------------------------
+
+
+
+
+# ------------------------------------------------------------------------------
+# EJERCICIO 7
+# Calcular el salario de un trabajador, ingresando las horas trabajadas y el
+# valor por hora, se debe mostrar el nombre del trabajador.
+# ------------------------------------------------------------------------------
+
+
+
+
+# ------------------------------------------------------------------------------
+# EJERCICIO 8
+# Un colegio desea saber qué porcentaje de niños y qué porcentaje de niñas hay
+# en el curso actual. Diseñar un algoritmo para este propósito (recuerda que
+# para calcular el porcentaje puedes hacer una regla de 3).
+# ------------------------------------------------------------------------------
+
+
+
+
+# ------------------------------------------------------------------------------
+# EJERCICIO 9
+# Ingresar un tiempo en segundos y separarlos en horas, minutos y segundos.
+# ------------------------------------------------------------------------------
+
+
+
+
+# ------------------------------------------------------------------------------
+# EJERCICIO 10
+# Dado un tiempo en segundos, calcular los segundos restantes que le
+# correspondan para convertirse exactamente en minutos.
+# ------------------------------------------------------------------------------
